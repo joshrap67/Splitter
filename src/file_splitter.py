ae61cb8fe0ebb89b950aa5eda97425a0138df617
joshrap67 <ghosthippo67@gmail.com>
2020-04-12 17:52:04 -0400

    Now linux might work?

--- src/file_splitter.py
+++ src/file_splitter.py
@@ -98,7 +98,7 @@ def make_file(directory, segment, min_wavelength, max_wavelength):
     maximum = int(max_wavelength)
     try:
         file_name = directory / ("%s_%s.txt" % (minimum, maximum))
-        output = open(file_name, "a+")
+        output = open(file_name, "w+")
         for i in range(0, len(segment), 1):
             if i is None:
                 break
